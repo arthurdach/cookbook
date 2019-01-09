8ca81aeb3eb41d729154e448ad604a32e3269788
arthur d achon <arthur.dachon@gmail.com>
2019-01-09 18:22:56 +0100

    navbar + controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+
+def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+    redirect_to recipe_path(@recipe)
+    else
+    render 'new'
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :recipes do
+      resources :reviews, only: [:new, :create, :show]
+      resources :doses, only: [:new, :create]
+    end
+    resources :doses, only: [:destroy]
+    resources :profiles, only: [:show, :edit, :update]
+
 end
