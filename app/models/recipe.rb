d1f2d99517b311e9f0bd8699aa3323182890579f
arthur d achon <arthur.dachon@gmail.com>
2019-01-11 18:20:32 +0100

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,16 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :doses
+  has_many :reviews
+
+  COURSE = ["Apero", "Starter", "Main Course", "Cheese", "Desert", "Digestive", "Golf", "Randonnée"].freeze
+  validates :course, inclusion: {
+      in: COURSE,
+
+    }
+
+  RESTRICTION = ["Vegan", "Fish", "Gluten-free", "Low-carb"].freeze
+  validates :restriction, inclusion: {
+      in: RESTRICTION,
+    }
 end
